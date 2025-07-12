b4a9ffee8a33a057ed4b5c3a05f7cb7383ca83c5
BENGONO AMVELA Nathan <nathanamvelabengono@gmail.com>
2025-07-12 10:08:02 +0100

    Update OpenAPIConfiguration.java

--- src/main/java/com/yowyob/dev/configuration/OpenAPIConfiguration.java
+++ src/main/java/com/yowyob/dev/configuration/OpenAPIConfiguration.java
@@ -16,7 +16,7 @@ public class OpenAPIConfiguration {
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
-        server.setUrl("http://157.90.26.3:8031");
+        server.setUrl("http://188.245.102.186:8031");
         server.setDescription("Development");
 
         Contact myContact = new Contact();
